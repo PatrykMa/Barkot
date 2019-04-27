a9ca4fee2b50c2cad465d8c9a449cfed44bdb018
pat <pat@pats-imac.local>
2019-04-27 06:59:50 -0700

    Initial Commit

---
+++ TableCellExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableCellExample
+//
+//  Created by pat on 27/04/2019.
+//  Copyright © 2019 pat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
